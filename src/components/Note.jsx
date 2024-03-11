5a6432906a04e9703210cf325a087a561e888043
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-03-11 15:13:02 +0800

    complete crud functionality in the back end

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,11 @@
-const Note = ({ note }) => {
+const Note = ({ note, toggleImportanceOf, deleteNote }) => {
   const label = note.important ? "make not important" : "make important";
 
   return (
     <li>
-      {note.content} <button>{label}</button>{" "}
+      {note.content}{" "}
+      <button onClick={() => toggleImportanceOf(note.id)}>{label}</button>{" "}
+      <button onClick={() => deleteNote(note.id)}>delete</button>
     </li>
   );
 };
---
+++ src/services/notes.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+const baseURl = "http://localhost:3001/notes";
+
+function getAllNotes() {
+  return axios.get(baseURl);
+}
+
+function createNote(newNote) {
+  return axios.post(baseURl, newNote);
+}
+
+function updateNote(id, newNote) {
+  return axios.put(`${baseURl}/${id}`, newNote);
+}
+
+function deleteNote(id) {
+  return axios.delete(`${baseURl}/${id}`);
+}
+
+export default {
+  getAllNotes,
+  createNote,
+  updateNote,
+  deleteNote,
+};
